87e814c2de284a0e5501637ea0fd2945014eb22b
Carson Payne <carson.j.payne14@gmail.com>
2025-03-16 18:36:08 -0500

    "Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,6 +11,7 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -18,6 +19,7 @@ class GameScene: SKScene {
         createBackground()
         resetGame()
         makePaddle()
+        makeBrick()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -55,13 +57,22 @@ class GameScene: SKScene {
         makeBall ()
     }
     func makePaddle() {
-    paddle.removeFromParent ()
-    // remove the paddle, if it exists
+        paddle.removeFromParent ()
+        // remove the paddle, if it exists
         paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
         paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
-    paddle.name = "paddle"
+        paddle.name = "paddle"
         paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
-    addChild(paddle)
-}
+        addChild(paddle)
+    }
+    func makeBrick() {
+        brick.removeFromParent()
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+    }
 }
